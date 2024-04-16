7f9e694ef2fb793f688a35c80c8aded1906fc2a8
RodrigoOliveira <rodrigooliveira.cont17@gmail.com>
2024-04-16 14:26:33 -0300

    refazendo navbar e adicionando primeiro card protótipo.

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,49 @@ let num = ref (0)
 </script>
 
 <template>
-    <!-- Navbar search -->
-    <nav class="navbar navbar-dark bg-primary">
-        <div class="container-fluid">
-            <router-link class="navbar-brand" to="/">
-                <img src="./assets/img/icons8-bullbasaur-48.png" alt="Logo">Mainodex
-            </router-link>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Buscar Pokemón" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-            </div>
-    </nav>
+    <nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+      
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <router-link class="navbar-brand" to="/">
+            <img src="./assets/img/icons8-bullbasaur-48.png" alt="">Mainodex
+        </router-link>
+        
+        <!-- Barra de pesquisa -->
+        <form class="d-flex gap-2" role="search">
+                    <input class="form-control form-group" type="search" placeholder="Buscar Pokemón" aria-label="Search">
+                    <button class="btn btn-success" type="submit">Buscar</button>
+        </form>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown link
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+
+</nav>
 
 
     <!-- Content -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,27 @@
 
 <template>
     <main>
-        Home
+        <div class="container">
+            
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <!-- Card -->
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://img.pokemondb.net/sprites/home/normal/bulbasaur.png" alt="Bulbasaur">
+                    <div class="card-body">
+                        <h5 class="card-title">Bulbassauro (RECEBER VIA API)</h5>
+                        <p class="card-text">Bulbasaur é um pequeno Pokémon anfíbio, principalmente turquesa, com olhos vermelhos e um bulbo verde nas costas. É baseado em um sapo/rã, com o bulbo parecido com o bulbo de uma planta que se transforma em uma flor à medida que evolui. (UTILIZAR API PARA DESCRIÇÃO)</p>
+                        <a href="#" class="btn btn-primary">Mais detalhes</a>
+                    </div>
+                    </div>
+
+                </div>
+
+                <div class="col-sm-12 col-md-6">
+                    teste
+                </div>
+            </div>
+        
+        </div>
     </main>
 </template>
