49a71eb5e2d15910d07f079afe556e20dcf4eecc
RodrigoOliveira <rodrigooliveira.cont17@gmail.com>
2024-04-16 22:16:42 -0300

    tentando adicionar os cards

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const pokemon = defineProps([ "name", "urlBasePng" ])
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card mb-3">
+            <p class="text-center">{{ pokemon.name }}</p>
+            <img 
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.png" 
+            alt="..." 
+            class="card-img-top"/>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,49 @@
 <script setup>
+    import { onMounted, reactive } from 'vue';
+    import ListPokemons from '../components/ListPokemons.vue';
+
+    let pokemons = reactive();
+
+    onMounted( ()=>{
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=150&offset=0")
+        .then( res => res.json() )
+        .then( res => pokemons = res.results );
+    })
+
 </script>
 
 <template>
     <main>
         <div class="container">
             
-            <div class="row">
+            <div class="row mt-4">
                 <div class="col-sm-12 col-md-6">
+
                     <!-- Card -->
+                    <!--
                     <div class="card" style="width: 18rem;">
                         <img src="https://img.pokemondb.net/sprites/home/normal/bulbasaur.png" alt="Bulbasaur">
                     <div class="card-body">
                         <h5 class="card-title">Bulbassauro (RECEBER VIA API)</h5>
-                        <p class="card-text">Bulbasaur é um pequeno Pokémon anfíbio, principalmente turquesa, com olhos vermelhos e um bulbo verde nas costas. É baseado em um sapo/rã, com o bulbo parecido com o bulbo de uma planta que se transforma em uma flor à medida que evolui. (UTILIZAR API PARA DESCRIÇÃO)</p>
+                        <p class="card-text">Bulbasaur é um pequeno Pokémon anfíbio, principalmente turquesa, com olhos vermelhos e um bulbo verde nas costas.(UTILIZAR API PARA DESCRIÇÃO)</p>
                         <a href="#" class="btn btn-primary">Mais detalhes</a>
                     </div>
-                    </div>
+                    </div>-->
+                    
+                    
 
                 </div>
 
                 <div class="col-sm-12 col-md-6">
-                    teste
+                    <div class="card">
+                        <div class="card-body row">
+                            <ListPokemons
+                            v-for="pokemon in pokemons"
+                            :key="pokemon.name"
+                            :name="pokemon.name"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         
